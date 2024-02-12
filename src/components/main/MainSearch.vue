3c585a656653b78022955643aae9b3096bdff77f
PierluigiPapa <pierluigipapa9@gmail.com>
2024-02-12 18:17:33 +0100

    fix: adjust code

--- src/components/main/MainSearch.vue
+++ src/components/main/MainSearch.vue
@@ -17,6 +17,7 @@ export default {
                 
                 store.arrayArchetype = res.data
             })
+             console.log("Elemento selezionato:", this.arrayArchetype)
         }
     },
     mounted () {
